5befe23184315cead78aad416b6f269f337aab95
SANAPALA VENKATA RAMANA <55456448+sv-ramana@users.noreply.github.com>
2021-01-07 14:38:57 +0530

    Add files via upload

---
+++ basic.py
@@ -0,0 +1,72 @@
+# print('hello world')
+
+# if 5 > 2:
+#     print('yes grater!')
+
+#     x = 5
+#     y = "venkat"
+#     print(x)
+#     print(y)
+
+#     x = 10
+#     x = 12
+#     print(x)
+
+
+#     x = str(3)
+#     y = float(4)
+#     z = int(5)
+#     print(x)
+#     print(y)
+#     print(z)
+
+#     a = 6
+#     b = "venkat"
+#     print(a)
+#     print(b)
+
+#     a = 5
+#     A = "venky"
+#     print(a)
+#     print(A)
+    
+
+#     d, e, f = "orange", "blue", "red"
+#     print(e)
+#     print(d)
+#     print(f)
+
+
+#     x = 5
+#     y = "svr"
+#     print(y + " is good boy")
+
+
+#     g = "svramana"
+
+#     def myfunc():
+#         print(g + " is a good boy")
+#         global h  #VERY IMPORTANT 
+#         h = " son"
+#     myfunc()
+#     print(h + " is a good boy")
+
+#     import random
+#     print(random.randrange(1, 10))
+
+#     x = 10
+#     print(float(x))
+#     print(complex(x))
+
+
+#     k = 1.00
+#     print(int(k))
+#     print(str(k))
+
+    l = "hello venkat"
+    print(b[2:2])
+
+
+
+
+
